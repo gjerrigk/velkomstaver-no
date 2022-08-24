05a0b9e9afc1942ff3a8ac776860ee903699ca5e
gjerrigk <post@gjerrigknark.com>
2022-08-24 12:47:17 +0200

    first commit

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: ['imagedelivery.net','cdn.sanity.io','app.gjerrigknark.com','gjerrigknark.com']
+  }
 }
 
 module.exports = nextConfig
